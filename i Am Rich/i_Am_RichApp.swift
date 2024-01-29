e945f886f2b751abda18891b638080d03d4d8a76
Shueib Abdirahman <136870666+shueibka@users.noreply.github.com>
2024-01-30 00:59:02 +0100

    Initial Commit

---
+++ i Am Rich/i_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_Am_RichApp.swift
+//  i Am Rich
+//
+//  Created by Fahad nur on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct i_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
